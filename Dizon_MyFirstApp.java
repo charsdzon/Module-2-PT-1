0ce306df4bd6cc20e6fc947df8a6174c189c5764
charsdzon <cmdizon@mcm.edu.ph>
2025-01-23 08:36:44 -0800

    Add files via upload

---
+++ Dizon_MyFirstApp.java
@@ -0,0 +1,8 @@
+public class MyFirstApp {
+
+    public static void main(String[] args){
+        System.out.println("Hello my name is Charles");
+        System.out.println("The current date and time is January 23, 2025 11:03 PM");
+    }
+    
+}
